ed3bbf0a17a26518c2fe66acba830b4352e7b409
monika003T <monikathakur2062@gmail.com>
2026-01-12 15:16:16 +0530

    created set and get user updated auth in services

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,9 @@
 const User= require("../models/user")
 const Url = require("../models/url")
+const { v4: uuidv4 } = require('uuid');
+const {setUser}= require("../service/auth")
+
+ 
 async function handleUserSignUp(req,res){
      console.log("BODY:", req.body);
     const {name,email,password} =req.body;
@@ -16,8 +20,13 @@ async function handleUserLogin(req,res){
     const {name,email,password} =req.body;
     const user= await User.findOne({email,password});
     if(!user) return res.render("login",{
-        error:"invalid username or password",});
-        return res.redirect('/');
+        error:"invalid username or password",
+    });
+
+    const sessionid=uuidv4();
+    setUser(sessionid,user);
+    res.cookie('uid',sessionid)
+    return res.redirect('/');
 }
 module.exports= {
     handleUserSignUp,
